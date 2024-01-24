0d3d09b456dff8dad50c533e867e778603089762
Mazhar Rasheed <mazharopal@gmail.com>
2024-01-24 17:24:07 +0500

    form validations

--- store/urls.py
+++ store/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from store import views
+
 from myshop import settings
+from store import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('signup/', views.signup,name='signup'),
+    path('signup', views.signup,name='signup'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
